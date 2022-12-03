b82cfe33e3e8909dd9c5f5768b629fe555689094
AdrienPoupa <adrien@poupa.net>
2022-12-03 02:43:12 -0500

    chore: Day 3 Refactoring, reduce number of steps

--- 2022/03/solution.ts
+++ 2022/03/solution.ts
@@ -10,58 +10,44 @@ const sumArray = function (array: number[]): number {
 /**
  * PART 1
  */
-
 const rucksacks = input.split('\n').filter((i: string) => i);
 
-// Split each rucksack in two equal compartments
-const compartments = rucksacks.map(
-    (content: string) => [content.slice(0, content.length / 2), content.slice(content.length / 2, content.length)]
-)
-
-// Deduplicate each compartment by using Sets
-const uniqueCompartments = compartments.map(
-    (content: string[]) => [new Set(content[0].split('')), new Set(content[1].split(''))]
+// Split each rucksack in two equal compartments, then deduplicate each compartment by using Sets
+const uniqueCompartments = rucksacks.map(
+    (content: string) => [
+        new Set(content.slice(0, content.length / 2).split('')),
+        new Set(content.slice(content.length / 2, content.length).split(''))
+    ]
 )
 
 // Find the intersection of the 2 sets
-const sharedItems = uniqueCompartments.map(
-    (content: Set<string>[]) => [...content[0]].filter(x => content[1].has(x))
-)
-    .flat() // Flatten the resulting array
+const sharedItems = uniqueCompartments.map((content: Set<string>[]) => [...content[0]].filter(x => content[1].has(x))).flat()
 
-// Convert each letter to its value
 // a = 97, so we need to subtract 96 to map it to 1
 // A = 65, so we need to subtract 38 to map it to 27
-const getLetterValues = function(sharedItems: string[]): number[] {
-    return sharedItems.map(
-        (item: string) => item.toLowerCase() === item ? (item.charCodeAt(0) - 96) : (item.charCodeAt(0) - 38)
-    )
+const getLetterValue = function (letter: string): number {
+    return letter.toLowerCase() === letter ? (letter.charCodeAt(0) - 96) : (letter.charCodeAt(0) - 38)
 }
 
-console.log('Sum of the priorities: ' + sumArray(getLetterValues(sharedItems)))
+console.log('Sum of the priorities: ' + sumArray(sharedItems.map((item: string) => getLetterValue(item))))
 
 /**
  * PART 2
  */
-
 // Create groups of 3 rucksacks
-let groupedRucksacks: string[][] = [];
+let uniqueGroupCompartments: Set<string>[][] = [];
 
 for (let i = 0; i < rucksacks.length; i = i + 3) {
-    groupedRucksacks.push([rucksacks[i], rucksacks[i + 1], rucksacks[i + 2]])
+    uniqueGroupCompartments.push([
+        new Set(rucksacks[i].split('')),
+        new Set(rucksacks[i + 1].split('')),
+        new Set(rucksacks[i + 2].split('')),
+    ])
 }
-
-const uniqueGroupCompartments = groupedRucksacks.map(
-    (content: string[]) => [
-        new Set(content[0].split('')),
-        new Set(content[1].split('')),
-        new Set(content[2].split(''))
-    ]
-)
 
 const sharedGroupItems = uniqueGroupCompartments.map(
     (content: Set<string>[]) => [...content[0]].filter(x => content[1].has(x) && content[2].has(x))
 )
     .flat()
 
-console.log('Sum of the priority groups: ' + sumArray(getLetterValues(sharedGroupItems)))
+console.log('Sum of the priority groups: ' + sumArray(sharedGroupItems.map((item: string) => getLetterValue(item))))
